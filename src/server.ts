36d2426956bc100f15d5908029804c3417b3ec28
harrison <d.harrison.lima@gmail.com>
2024-07-14 15:32:28 -0300

    feat: add order, gender e language movie

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,18 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get("/movies", async (req, res) => {
-  const movie = await prisma.movie.findMany();
+  const movie = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    include: {
+      genres: true,
+      languages: true,
+    },
+  });
   res.json(movie);
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
